aaf2b52d437db39559211982f7708701a2d4b55a
stevenfen <634606228@qq.com>
2016-09-06 19:43:58 +0800

    2016-9-6 CTVE笔试题目更新

---
+++ src/CVTE/second/Main.java
@@ -0,0 +1,34 @@
+package CVTE.second;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner in =new Scanner(System.in);
+        while(in.hasNext()){
+        	char[] letter = in.nextLine().toCharArray();
+        	Stack<String> stack = new Stack<>();
+        	for(int i=0;i<letter.length;i++){
+        		if(letter[i]=='{'){
+        			stack.push("{");
+        		}else if(letter[i]=='('){
+        			stack.push("(");
+        		}else if(letter[i]=='}'){
+        			if(stack.peek()=="{"){
+        				stack.pop();
+        			}
+        		}else if(letter[i]==')'){
+        			if(stack.peek()=="("){
+        				stack.pop();
+        			}
+        		}
+        	}
+        	System.out.println( stack.isEmpty());
+        	
+        }
+	}
+	
+}
